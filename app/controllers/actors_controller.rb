3f68720219f778e1d53d36f8b24145a654da093e
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-02-09 19:27:52 +0000

    added actors#create

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,20 @@
 class ActorsController < ApplicationController
+  def create
+    #Retrieve the user's inputs from params
+    #Create a record in the movie table
+    #Populate each column with the user input
+    #Save
+    #
+    a = Actor.new
+    a.name = params.fetch("the_name")
+    a.dob = params.fetch("the_dob")
+    a.bio = params.fetch("the_bio")
+    a.image = params.fetch("the_image")
+
+    a.save
+    redirect_to("/actors")
+  end
+
   def index
     matching_actors = Actor.all
     @list_of_actors = matching_actors.order({ :created_at => :desc })
